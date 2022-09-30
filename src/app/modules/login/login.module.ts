9c7ee66dd3a6f75019556eedbe63fdd02952fe0c
Maye Edwin <mayedwine1@gmail.com>
2022-10-01 00:14:22 +0300

    Add signin, signup views aka templates

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  // Properties.
+  loginForm: FormGroup;
+  signupForm: FormGroup;
+  showLogin: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    // Set up the login form.
+    this.loginForm = this.fb.group({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+    // Setup signup form.
+    this.signupForm = this.fb.group({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
+    });
   }
 
+  ngOnInit(): void {}
 }
