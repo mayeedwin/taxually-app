8d76ee09e854af223e889c4e48bad1349bc5cae7
Maye Edwin <mayedwine1@gmail.com>
2022-10-02 01:18:12 +0300

    Add tiny jest test case to home component

--- src/app/modules/home/home/home.component.spec.ts
+++ src/app/modules/home/home/home.component.spec.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
+  let dom: any;
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
@@ -20,9 +21,39 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dom = fixture.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test(`should have 'Taxually.com' size alert `, () => {
+    fixture.detectChanges();
+    expect(dom.innerHTML).toContain('Taxually.com');
+    expect(dom.innerHTML).toContain('Note : Max file size for upload is 1MB');
+  });
+
+  test(`should have file input`, () => {
+    fixture.detectChanges();
+    // Check it has an file input.
+    expect(dom.querySelector('input[type="file"]')).toBeTruthy();
+  });
+
+  test(`renders a list of photo item`, () => {
+    fixture.detectChanges();
+    // Check it has an element with class 'photo' after 5 seconds.
+    setTimeout(() => {
+      expect(dom.querySelector('.photo')).toBeTruthy();
+    }, 4000);
+  });
+
+  test(`all img tags should have src attribute and alt attribute`, () => {
+    fixture.detectChanges();
+    // Check it has an element with class 'photo' after 5 seconds.
+    setTimeout(() => {
+      const images = dom.querySelectorAll('img');
+      // Check if all images have src and alt attributes.
+      images.forEach((image: any) => {
+        expect(image.src).toBeTruthy();
+        expect(image.alt).toBeTruthy();
+      });
+    }, 5000);
   });
 });
