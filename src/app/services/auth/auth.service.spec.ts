3d438e78f7b01b26a6757b4c4e99620ab360c37f
Maye Edwin <mayedwine1@gmail.com>
2022-10-02 14:03:45 +0300

    Add stoarge service tests

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from 'src/app/modules/login/login/login.component';
 
 import { AuthService } from './auth.service';
 
@@ -8,12 +10,34 @@ describe('AuthService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: LoginComponent },
+        ]),
+      ],
     });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('user can be authenticated', () => {
+    const user = {
+      email: 'me@hmail.com',
+      password: '123456',
+    };
+    const res = service.login(user.email, user.password);
+    expect(res.message).toBe('Sorry, user does not exist');
+  });
+
+  test('user can be logged out', () => {
+    service.logout();
+  });
+
+  test('can get loggedin status', () => {
+    const res = service.isLoggedIn();
+    expect(res).toBe(false);
   });
 });
--- src/app/services/storage/storage.service.spec.ts
+++ src/app/services/storage/storage.service.spec.ts
@@ -4,7 +4,19 @@ import { StorageService } from './storage.service';
 
 describe('StorageService', () => {
   let service: StorageService;
-
+  // Set initial data.
+  const initial = [
+    {
+      id: '1',
+      url: 'https://www.google.com',
+      title: 'Google',
+      albumId: 1,
+      userId: '1',
+      createdAt: '2020-01-01',
+      size: 100,
+      sizeInKb: 100,
+    },
+  ];
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StorageService);
@@ -12,5 +24,23 @@ describe('StorageService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('can get data & save data', () => {
+    // Get saved data.
+    const res = service.getSavedPhotos();
+    expect(res).toEqual([]);
+    // Save data.
+    service.setStorage(initial);
+    // Get saved data again.
+    const res2 = service.getSavedPhotos();
+    expect(res2).toEqual(initial);
+  });
+
+  test('can save and delete a photo', () => {
+    // Save data.
+    service.saveImage(initial[0], 'em@taxually.com');
+    // Delete data.
+    service.deletePhoto(initial[0].id, 'em@taxually.com');
   });
 });
