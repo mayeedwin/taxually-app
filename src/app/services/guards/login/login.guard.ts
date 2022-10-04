6e3e4ce0db1062f6f721508e9723952cd48a06bb
Maye Edwin <mayedwine1@gmail.com>
2022-10-04 12:41:06 +0300

    Cleanup code, remove unsused code

--- src/app/services/guards/login/login.guard.ts
+++ src/app/services/guards/login/login.guard.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 
@@ -14,10 +8,7 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class LoginGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
+  canActivate():
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree>
     | boolean
